54cd3476dc5465b9fbdb67041c311cc147ee6a92
adolfo.rodrigues <adolfo.rodrigues@cnj.jus.br>
2023-11-27 22:32:18 -0300

    Ajuste de cores menu header

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,10 +22,6 @@
 						<span class="icon-bar"></span>
 					</button>
 					
-					<div class="search-btn">
-						<a href="javascript:void(0);" class="search-trigger"><i class="icon-icons185"></i></a>
-					</div>
-					
 					<div class="search-container">
 						<i class="fa fa-times header-search-close"></i>
 						<div class="search-overlay"></div>
