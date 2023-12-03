c58f7c2ba5336a0da9bc6c6148a77ece22c1749b
adolfo.rodrigues <adolfo.rodrigues@cnj.jus.br>
2023-12-03 12:43:57 -0300

    Correção de baner home, styles css

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -47,6 +47,7 @@
 			<div class="tp-banner-container">
 				<div class="col-md-12 tp-banner" >
 					<ul>
+						{{-- left --}}
 						<li data-transition="fade" data-slotamount="5" data-masterspeed="300" data-thumb="images/main-banner/2/1.jpg"  data-saveperformance="on"  data-title="">
 								
 							<!-- MAIN IMAGE -->
@@ -62,26 +63,13 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 2;">Money Doesn’t
+								style="z-index: 2;"><span class="color-default">Money Doesn't Come Without Care</span>
 							</div>
 				
 							<!-- LAYER NR. 2 -->
-							<div class="tp-caption banner-heading lft rs-parallaxlevel-0"
-								data-x="0"
-								data-y="270" 
-								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-								data-speed="300"
-								data-start="500"
-								data-easing="Power3.easeInOut"
-								data-elementdelay="0.1"
-								data-endelementdelay="0.1"
-								style="z-index: 2;">Come <span class="color-default">Without Care</span>
-							</div>
-				
-							<!-- LAYER NR. 3 -->
 							<div class="tp-caption lft banner-text rs-parallaxlevel-0"
 								data-x="0"
-								data-y="340" 
+								data-y="300" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
 								data-speed="300"
 								data-start="1000"
@@ -94,20 +82,20 @@
 							<!-- LAYER NR. 4 -->
 							<div class="tp-caption banner-btn colored lft banner-btn rs-parallaxlevel-0"
 								data-x="0"
-								data-y="390" 
+								data-y="350" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
 								data-speed="300"
 								data-start="1500"
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 4;"><a href="#.">read more</a>
+								style="z-index: 4;"><a href="#.">Leia Mais</a>
 							</div>
 				
 							<!-- LAYER NR. 5 -->
 							<div class="tp-caption banner-btn lft banner-btn rs-parallaxlevel-0"
 								data-x="180"
-								data-y="390" 
+								data-y="350" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
 								data-speed="300"
 								data-start="1500"
@@ -118,15 +106,14 @@
 							</div>
 								
 						</li>
-				
+						{{-- center --}}
 						<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-thumb="images/main-banner/2/2.jpg"  data-saveperformance="on"  data-title="">
 								
 							<!-- MAIN IMAGE -->
 							<img src="images/main-banner/2/2.jpg"  alt="" data-lazyload="images/main-banner/2/2.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-					
-								
+
 							<!-- LAYER NR. 1 -->
-							<div class="tp-caption banner-heading color-white lft rs-parallaxlevel-0"
+							<div class="tp-caption banner-heading color-white center rs-parallaxlevel-0"
 								data-x="center"
 								data-y="210" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -135,11 +122,11 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 2;">Clients <span class="color-default">Investment</span> Guidance
+								style="z-index: 2;">Clients Investment Guidance
 							</div>
 				
 							<!-- LAYER NR. 2 -->
-							<div class="tp-caption lft banner-text color-white rs-parallaxlevel-0"
+							<div class="tp-caption center banner-text color-white rs-parallaxlevel-0"
 								data-x="center"
 								data-y="288" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -148,24 +135,11 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 3;">With over 15 years of experience we’ll ensure you always get the best guidance
+								style="z-index: 3;">With over 15 years of experience we’ll ensure you always get the best guidance we’re with you every step of the way
 							</div>
 					
 							<!-- LAYER NR. 3 -->
-							<div class="tp-caption lft banner-text color-white rs-parallaxlevel-0"
-								data-x="center"
-								data-y="315" 
-								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-								data-speed="300"
-								data-start="1000"
-								data-easing="Power3.easeInOut"
-								data-elementdelay="0.1"
-								data-endelementdelay="0.1"
-								style="z-index: 3;">we’re with you every step of the way
-							</div>
-				
-							<!-- LAYER NR. 3 -->
-							<div class="tp-caption banner-btn colored lft banner-btn rs-parallaxlevel-0"
+							<div class="tp-caption banner-btn colored center banner-btn rs-parallaxlevel-0"
 								data-x="400"
 								data-y="370" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -174,11 +148,11 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 4;"><a href="#.">read more</a>
+								style="z-index: 4;"><a href="#.">leia mais</a>
 							</div>
 					
 							<!-- LAYER NR. 5 -->
-							<div class="tp-caption banner-btn lft banner-btn rs-parallaxlevel-0"
+							<div class="tp-caption banner-btn center banner-btn rs-parallaxlevel-0"
 									data-x="580"
 									data-y="370" 
 									data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -187,17 +161,17 @@
 									data-easing="Power3.easeInOut"
 									data-elementdelay="0.1"
 									data-endelementdelay="0.1"
-									style="z-index: 4;"><a href="#.">Contact Us</a>
+									style="z-index: 4;"><a href="#.">Fale Conosco</a>
 								</div>
 						</li>
-				
+						{{-- right --}}
 						<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-thumb="images/main-banner/2/3.jpg"  data-saveperformance="on"  data-title="">
 								
 							<!-- MAIN IMAGE -->
 							<img src="images/main-banner/2/3.jpg"  alt="" data-lazyload="images/main-banner/2/3.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 				
 							<!-- LAYER NR. 1 -->
-							<div class="tp-caption banner-heading lft rs-parallaxlevel-0"
+							<div class="tp-caption banner-heading right rs-parallaxlevel-0"
 								data-x="right"
 								data-y="210" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -206,11 +180,11 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 2;">Friendly <span class="color-default">Assistance</span>
+								style="z-index: 2;"><span class="color-default">Friendly Assistance</span>
 							</div>
 				
 							<!-- LAYER NR. 2 -->
-							<div class="tp-caption lft banner-text rs-parallaxlevel-0"
+							<div class="tp-caption right banner-text rs-parallaxlevel-0"
 								data-x="right"
 								data-y="288" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
@@ -219,24 +193,11 @@
 								data-easing="Power3.easeInOut"
 								data-elementdelay="0.1"
 								data-endelementdelay="0.1"
-								style="z-index: 3;">With over 15 years of experience we’ll ensure you always get the best
+								style="z-index: 3;">With over 15 years of experience we'll ensure you always get the best guidance. we're with you every step of the way
 							</div>
 				
 							<!-- LAYER NR. 3 -->
-							<div class="tp-caption lft banner-text rs-parallaxlevel-0"
-								data-x="right"
-								data-y="315" 
-								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-								data-speed="300"
-								data-start="1000"
-								data-easing="Power3.easeInOut"
-								data-elementdelay="0.1"
-								data-endelementdelay="0.1"
-								style="z-index: 3;">guidance. we’re with you every step of the way
-							</div>
-				
-							<!-- LAYER NR. 3 -->
-							<div class="tp-caption banner-btn colored lft banner-btn rs-parallaxlevel-0"
+							<div class="tp-caption banner-btn colored right banner-btn rs-parallaxlevel-0"
 								data-x="right"
 								data-y="370" 
 								data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
