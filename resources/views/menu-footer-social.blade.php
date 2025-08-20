e2ba84e75d3d244ed34b049cfcd66f328782000b
AdolfoAugustoR <adolfo.rodrigues@cnj.jus.br>
2025-08-20 09:38:03 -0300

    Ajuste no menu

--- resources/views/menu-footer-social.blade.php
+++ resources/views/menu-footer-social.blade.php
@@ -1,25 +1,21 @@
 @foreach($items as $item)
-   @if ($item->title =='facebook')
-      <li class="animate bounceIn">
-         <a href="{{ $item->link() }}" target="{{ $item->target }}" class="facebook"><i class="icon-facebook-1"></i></a>
-      </li>
-   @elseif ($item->title =='twitter')
-      <li class="animate bounceIn" data-delay="100">
-         <a href="{{ $item->link() }}" target="{{ $item->target }}" class="twitter"><i class="icon-twitter-1"></i></a>
-      </li>
-   @elseif ($item->title =='google-plus')
-      <li class="animate bounceIn" data-delay="200">
-         <a href="{{ $item->link() }}" target="{{ $item->target }}" class="google-plus"><i class="icon-google"></i></a>
-      </li>
-   @elseif ($item->title =='linkedin')
-      <li class="animate bounceIn" data-delay="300">
-         <a href="{{ $item->link() }}" target="{{ $item->target }}" class="linkedin"><i class="icon-linkedin3"></i></a>
-      </li>
-   @else
-      <li class="animate bounceIn" data-delay="400">
-         <a href="{{ $item->link() }}" target="{{ $item->target }}" class="{{ $item->title }}">
-            <i class="{{ $item->icon_class }}"></i>
-         </a>
-      </li>
-   @endif
+    @if ($item->title =='facebook')
+        <li class="animate bounceIn" data-delay="100">
+            <a href="{{ $item->link() }}" target="{{ $item->target }}" class="twitter"><i class="fa fa-instagram"></i></a>
+        </li>
+    @elseif ($item->title =='twitter')
+        <li class="animate bounceIn">
+            <a href="{{ $item->link() }}" target="{{ $item->target }}" class="facebook"><i class="icon-facebook-1"></i></a>
+        </li>
+    @elseif ($item->title =='linkedin')
+        <li class="animate bounceIn" data-delay="300">
+            <a href="{{ $item->link() }}" target="{{ $item->target }}" class="linkedin"><i class="icon-linkedin3"></i></a>
+        </li>
+    @else
+        <li class="animate bounceIn" data-delay="400">
+            <a href="{{ $item->link() }}" target="{{ $item->target }}" class="{{ $item->title }}">
+                <i class="{{ $item->icon_class }}"></i>
+            </a>
+        </li>
+    @endif
 @endforeach
